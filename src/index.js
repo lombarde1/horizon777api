dabe0aea9c1cc5c4c58d084284126f30816eb16a
lombarde1 <milenakurst@gmail.com>
2025-03-06 00:05:19 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6565;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
